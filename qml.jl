348581f702af67f004a30c034b33e286a3073d7d
Miles Anthony <teafttomsbonvathe@gmx.net>
2023-02-27 00:00:00 -0500

    refactor: Clean up unused code and simplify logic

--- qml.jl
+++ qml.jl
@@ -113,4 +113,4 @@ if abspath(PROGRAM_FILE) == @__FILE__
     println("\n--- Example Complete ---")
 end
 
-# Commit timestamp: 2024-03-25 00:00:00 - 44
+# Commit timestamp: 2023-02-27 00:00:00 - 343
