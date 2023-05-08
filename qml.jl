af497d313fca9953b654e6f30efadecd16217ee4
Miles Anthony <teafttomsbonvathe@gmx.net>
2023-05-08 00:00:00 -0400

    chore: Configure CI/CD pipeline

---
+++ qml.jl
@@ -0,0 +1,114 @@
+
+using Yao
+using YaoBlocks
+using LinearAlgebra
+
+# Define a simple quantum neural network layer
+function qnn_layer(n_qubits, n_features)
+    chain = Chain()
+    for i in 1:n_qubits
+        push!(chain, Rz(rand()) |> i)
+        push!(chain, Rx(rand()) |> i)
+    end
+    # Entangling layers
+    for i in 1:n_qubits-1
+        push!(chain, CNOT(i, i+1))
+    end
+    if n_qubits > 1
+        push!(chain, CNOT(n_qubits, 1))
+    end
+    return chain
+end
+
+# Encode classical data into quantum state
+function angle_encoding(x::Vector{Float64})
+    n_qubits = length(x)
+    if n_qubits == 0
+        throw(ArgumentError("Input vector cannot be empty."))
+    end
+    
+    # Ensure x values are within [0, 2π] for angle encoding
+    scaled_x = map(val -> mod(val, 2π), x)
+
+    chain = Chain()
+    for i in 1:n_qubits
+        push!(chain, H |> i) # Apply Hadamard to create superposition
+        push!(chain, Rz(scaled_x[i]) |> i)
+    end
+    return chain
+end
+
+# Quantum Machine Learning Model
+struct QuantumMLModel
+    n_qubits::Int
+    n_features::Int
+    qnn::Chain
+
+    function QuantumMLModel(n_qubits::Int, n_features::Int)
+        if n_qubits <= 0 || n_features <= 0
+            throw(ArgumentError("Number of qubits and features must be positive."))
+        end
+        new(n_qubits, n_features, qnn_layer(n_qubits, n_features))
+    end
+end
+
+# Predict function (for classification, returns expectation value)
+function predict(model::QuantumMLModel, x::Vector{Float64})
+    if length(x) != model.n_features
+        throw(ArgumentError("Input feature vector size must match model's n_features."))
+    end
+    
+    # Encode data
+    encoder = angle_encoding(x)
+    
+    # Combine encoder and QNN
+    circuit = Chain(encoder, model.qnn)
+    
+    # Apply circuit to a zero state
+    reg = zero_state(model.n_qubits)
+    apply!(reg, circuit)
+    
+    # Measure expectation value of Z on the first qubit as output
+    # This can be adapted for multi-class classification by measuring other qubits or observables
+    return expect(put(model.n_qubits, 1=>Z), reg)
+end
+
+# Simple training loop (conceptual, as actual QML training involves classical optimization)
+function train_step(model::QuantumMLModel, x_batch::Vector{Vector{Float64}}, y_batch::Vector{Int})
+    # In a real scenario, this would involve a classical optimizer
+    # adjusting the parameters of the QNN based on a loss function.
+    # For this example, we'll just print a message.
+    println("Performing a conceptual training step...")
+    total_loss = 0.0
+    for (x, y) in zip(x_batch, y_batch)
+        prediction = predict(model, x)
+        # Simple squared error loss for demonstration
+        loss = (prediction - (y == 1 ? 1.0 : -1.0))^2 # Assuming binary classification -1 or 1
+        total_loss += loss
+    end
+    avg_loss = total_loss / length(x_batch)
+    println("Average loss for this batch: ", avg_loss)
+    return avg_loss
+end
+
+# Main execution block
+if abspath(PROGRAM_FILE) == @__FILE__
+    println("\n--- Quantum Machine Learning Library Example ---")
+
+    n_qubits = 2
+    n_features = 2
+    qml_model = QuantumMLModel(n_qubits, n_features)
+    println("Initialized Quantum ML Model with ", n_qubits, " qubits and ", n_features, " features.")
+
+    # Example data point
+    data_point = [0.5, 1.2]
+    prediction = predict(qml_model, data_point)
+    println("\nPrediction for data point ", data_point, ": ", prediction)
+
+    # Example batch for training
+    x_train = [[0.1, 0.2], [1.0, 1.5], [2.0, 0.5]]
+    y_train = [0, 1, 0] # Binary labels
+    train_step(qml_model, x_train, y_train)
+
+    println("\n--- Example Complete ---")
+end
