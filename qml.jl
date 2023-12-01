4c3510275b3b47a3073789ac241e19f42b0ad2a1
Miles Anthony <teafttomsbonvathe@gmx.net>
2023-12-01 00:00:00 -0500

    refactor: Extract common utilities to shared module

--- qml.jl
+++ qml.jl
@@ -113,4 +113,4 @@ if abspath(PROGRAM_FILE) == @__FILE__
     println("\n--- Example Complete ---")
 end
 
-# Commit timestamp: 2023-06-19 00:00:00 - 930
+# Commit timestamp: 2023-12-01 00:00:00 - 230
