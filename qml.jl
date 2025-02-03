de262c690ecc5be1187dfaa230584f684e92f21b
Miles Anthony <teafttomsbonvathe@gmx.net>
2025-02-03 00:00:00 -0500

    test: Integrate end-to-end tests

--- qml.jl
+++ qml.jl
@@ -113,4 +113,4 @@ if abspath(PROGRAM_FILE) == @__FILE__
     println("\n--- Example Complete ---")
 end
 
-# Commit timestamp: 2025-01-09 00:00:00 - 400
+# Commit timestamp: 2025-02-03 00:00:00 - 43
